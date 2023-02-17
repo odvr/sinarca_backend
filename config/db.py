72345056643ad3a2b5c79f6243ef045fdfa5a1c9
David Vega <omar.vegar@cun.edu.co>
2023-02-17 14:09:25 -0500

    ejecución de la API para inventarios.

--- config/db.py
+++ config/db.py
@@ -1,17 +1,25 @@
+'''
+@autor: odvr
 
+El siguiente codigo realiza la conexion de la base de datos para mariaDB
+
+'''
 #Librerias requeridas
 from  sqlalchemy import create_engine,MetaData
 import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
+
 """
-create_engine requiere una URL
-donde se encuentren las credenciales 
-Registra las credenciales de la base de datos ejempo mysql+pymysql://usuario:password@rutaDB
+Registra tu base de datos siguiendo los parametros en el ejemplo:
+mariadb+mariadbconnector://usuario:password@IPbasededatos:puerto/nombreBasededatos"
 """
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:1q2w3e4r@localhost:3306/sinarca")
-meta = MetaData()
-#llama esta variable cuando quieras interactuar con la base de datos
-condb = engine.connect()
+try:
+    engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:1q2w3e4r@localhost:3306/sinarca")
+    meta = MetaData()
+    # llama esta variable cuando quieras interactuar con la base de datos
+    condb = engine.connect()
+except:
+    print("Valida las credenciales de la base de datos"+condb)
+
 
 
 
