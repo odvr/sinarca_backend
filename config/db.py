a07ef4795698bc44eb1c1d97504b28619314998f
David Vega <omar.vegar@cun.edu.co>
2023-09-14 08:53:38 -0500

    Ajuste de conexion para la base de datos

--- config/db.py
+++ config/db.py
@@ -13,10 +13,10 @@ from sqlalchemy.orm import sessionmaker
 Registra tu base de datos siguiendo los parametros en el ejemplo:
 mariadb+mariadbconnector://usuario:password@IPbasededatos:puerto/nombreBasededatos"
 """
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:1q2w3e4r@localhost:3306/sinarcas")
+engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:1q2w3e4r@sinarcadb.cc3ho0ocanpp.us-east-1.rds.amazonaws.com:3306/sinarca")
 meta = MetaData()
 def get_session():
-    engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:1q2w3e4r@localhost:3306/sinarcas")
+    engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:1q2w3e4r@sinarcadb.cc3ho0ocanpp.us-east-1.rds.amazonaws.com:3306/sinarca")
     try:
 
         Session = sessionmaker(bind=engine)
