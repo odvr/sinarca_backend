dad1dbd1fa9bf31f6d412184c4e2222ef7b96e8c
David Vega <omar.vegar@cun.edu.co>
2023-10-12 17:32:02 -0500

    Actualizacion de Origenes

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ en las que una interfaz que se ejecuta en un navegador tiene código JavaScript 
 '''
 app = FastAPI()
 
-origins = ["https://sinarca.co"]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
