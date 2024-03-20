25e3d8ed7d9017b83b2b2e4488be516a5b108820
David Vega <omar.vegar@cun.edu.co>
2024-03-20 12:02:48 -0500

    Mejora: Se ajusta la api para una sola consulta

--- routes/Historial_tasa_supervivencia_Bovinos.py
+++ routes/Historial_tasa_supervivencia_Bovinos.py
@@ -7,10 +7,12 @@ import logging
 from fastapi import APIRouter, Response
 from sqlalchemy.orm import Session
 
+from Lib.natalidad_paricion_real import natalidad_paricion_real
 # importa la conexion de la base de datos
 from config.db import get_session
 # importa el esquema de los bovinos
-from models.modelo_bovinos import modelo_historial_perdida_terneros, modelo_historial_supervivencia
+from models.modelo_bovinos import modelo_historial_perdida_terneros, modelo_historial_supervivencia, \
+    modelo_natalidad_paricion_real
 
 from routes.rutas_bovinos import get_current_user
 from schemas.schemas_bovinos import esquema_historial_perdida_terneros, Esquema_Usuario, esquema_historial_supervivencia
@@ -56,18 +58,51 @@ def get_database_session():
     finally:
         db.close()
 
-@Historial_Tasa_Supervivencia.get("/listar_tabla_historial_tasa_supervivencia",response_model=list[esquema_historial_supervivencia])
+@Historial_Tasa_Supervivencia.get("/listar_tabla_historial_tasa_supervivencia",response_model=list)
 async def listar_tabla_perdida_terneros(db: Session = Depends(get_database_session),current_user: Esquema_Usuario = Depends(get_current_user)):
     try:
 
-
+        natalidad_paricion_real(session=db, current_user=current_user)
+        itemsListarNtalidades = db.query(modelo_natalidad_paricion_real).filter(
+            modelo_natalidad_paricion_real.c.usuario_id == current_user).all()
         items_tasa_supervivencia = db.query(modelo_historial_supervivencia).filter(modelo_historial_supervivencia.c.usuario_id == current_user).all()
         db.close()
+        # Declara la Variable que contiene una lista
+        HistorialTasaParicionReal = []
+        #Valida si la consulta no esta vacia
+        if itemsListarNtalidades is not None:
+            for Natalidad in itemsListarNtalidades:
+                #define la validable para agregar la lista y ingresa el diccionario de la información
+                ItemsNatalidad = {
+                    "periodoNatalidad": Natalidad.periodo,
+                    "intervalo_entre_partos_periodo": Natalidad.intervalo_entre_partos_periodo,
+                    "natalidad_paricion_real": Natalidad.natalidad_paricion_real,
 
-        return items_tasa_supervivencia
+                }
+
+                HistorialTasaParicionReal.append(ItemsNatalidad)
+        else:
+            pass
+        if items_tasa_supervivencia is not None:
+            for supervivencia in items_tasa_supervivencia:
+                # define la validable para agregar la lista
+                ItemsSupervivencia = {
+                    "periodo": supervivencia.periodo,
+                    "supervivencia": supervivencia.supervivencia,
+
+
+                }
+                print(ItemsSupervivencia)
+                HistorialTasaParicionReal.append(ItemsSupervivencia)
+
+
+
+
+        # Retorna la lista
+        return HistorialTasaParicionReal
 
     except Exception as e:
-        logger.error(f'Error al obtener Listar REGISTRO DE TASA SUPERVIVENCIA: {e}')
+        logger.error(f'Error al obtener Listar REGISTRO DE TASA SUPERVIVENCIA y Natalidad: {e}')
         raise
     finally:
         db.close()
