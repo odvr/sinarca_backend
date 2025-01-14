4cb58e1fa4f3f9d3d4ffec67e8feba8d2549dc88
odvr <omar.vegar@cun.edu.co>
2025-01-14 18:38:48 -0500

    Versión Compatible FrondEnd 1.5.2: Bug: Se realiza ajuste en la creación Mejora: En la creación de Bovinos

--- Lib/Cambiar_Estado_Facturas.py
+++ Lib/Cambiar_Estado_Facturas.py
@@ -23,41 +23,43 @@ def CambiarEstadoFactura():
     try:
         # Obtener la fecha actual
         FechaActual = datetime.now().date()
-
-
         # Consultar facturas que no han vencido
-        ConsultarFechaFactura = session.query(modelo_facturas).\
-            filter(modelo_facturas.c.fecha_vencimiento >= FechaActual).all()
+        ConsultarFechaFactura = session.query(modelo_facturas).all()
 
-        # Actualizar el estado de las facturas vencidas
-        for Factura in ConsultarFechaFactura:
+        for factura in ConsultarFechaFactura:
+            fechaVencimiento = factura.fecha_vencimiento
+            estadoFactura = factura.estado
+            if fechaVencimiento < FechaActual and estadoFactura == "pendiente":
+                # Consultar facturas que no han vencido
+                ConsultarFechaFactura = session.query(modelo_facturas). \
+                    filter(modelo_facturas.c.fecha_vencimiento >= FechaActual).all()
 
-            session.execute(
-                modelo_facturas.update().values(
-                    estado="vencida"
-                ).where(
-                    modelo_facturas.c.factura_id == Factura.factura_id
-                )
-            )
-            #ID Usuario a Notificar
-            IdUsuario = Factura.usuario_id
+                # Actualizar el estado de las facturas vencidas
+                for Factura in ConsultarFechaFactura:
+                    session.execute(
+                        modelo_facturas.update().values(
+                            estado="vencida"
+                        ).where(
+                            modelo_facturas.c.factura_id == Factura.factura_id
+                        )
+                    )
+                    # ID Usuario a Notificar
+                    IdUsuario = Factura.usuario_id
 
-            CorreoElectronico = crud.bovinos_inventario.Buscar_Correo_Usuario(db=session,usuario_id=IdUsuario)
+                    CorreoElectronico = crud.bovinos_inventario.Buscar_Correo_Usuario(db=session, usuario_id=IdUsuario)
 
-            "El dato que Retorna es tipo Byte se realiza la conversión"
-            Correo = str(CorreoElectronico, encoding='utf-8')
+                    "El dato que Retorna es tipo Byte se realiza la conversión"
+                    Correo = str(CorreoElectronico, encoding='utf-8')
 
-            #Radicado de Factura para Notificar
-            RadicadoFactura = str( Factura.radicado_factura, encoding='utf-8')
+                    # Radicado de Factura para Notificar
+                    RadicadoFactura = str(Factura.radicado_factura, encoding='utf-8')
 
+                    # Envia La notificación
+                    enviar_correo(Correo, "Notificación de Vencimiento de Factura",
+                                  "Estimado/a, le informamos que la factura con número de radicado " + RadicadoFactura + " ha vencido.")
 
+                session.commit()
 
-            #Envia La notificación
-            enviar_correo(Correo, "Notificación de Vencimiento de Factura",
-                          "Estimado/a, le informamos que la factura con número de radicado " + RadicadoFactura + " ha vencido.")
-
-
-        session.commit()
     except Exception as e:
         # Revertir cambios si ocurre un error
         session.rollback()
@@ -77,21 +79,23 @@ def CambiarEstadoFacturaSinEnviarNotificacion():
         FechaActual = datetime.now().date()
 
         # Consultar facturas que no han vencido
-        ConsultarFechaFactura = session.query(modelo_facturas). \
-            filter(modelo_facturas.c.fecha_vencimiento >= FechaActual).all()
+        ConsultarFechaFactura = session.query(modelo_facturas).all()
 
-        # Actualizar el estado de las facturas vencidas
-        for Factura in ConsultarFechaFactura:
 
-            session.execute(
-                modelo_facturas.update().values(
-                    estado="vencida"
-                ).where(
-                    modelo_facturas.c.factura_id == Factura.factura_id
+        for factura in ConsultarFechaFactura:
+            fechaVencimiento = factura.fecha_vencimiento
+            estadoFactura  =  factura.estado
+            if fechaVencimiento < FechaActual and  estadoFactura == "pendiente":
+                session.execute(
+                    modelo_facturas.update().values(
+                        estado="vencida"
+                    ).where(
+                        modelo_facturas.c.factura_id == factura.factura_id
+                    )
                 )
-            )
 
-        session.commit()
+                session.commit()
+
     except Exception as e:
         # Revertir cambios si ocurre un error
         session.rollback()
